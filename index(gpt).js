a426649531b71544a0e6b09e480009205d853f95
chezeef <97364291+chezeef@users.noreply.github.com>
2022-12-16 20:13:30 -0800

    wes

--- index(gpt).js
+++ index(gpt).js
@@ -1,37 +1,117 @@
-const square = document.getElementById("square");
 
-document.addEventListener("keydown", handleKeyDown);
 
-function handleKeyDown(event) {
-  switch (event.key) {
-    case "ArrowLeft":
-      // Move the square to the left
-      break;
-    case "ArrowRight":
-      // Move the square to the right
-      break;
-    case "ArrowUp":
-      // Move the square up
-      break;
-    case "ArrowDown":
-      // Move the square down
-      break;
-  }
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+canvas.width = 1024
+canvas.height = 576
+
+c.fillRect(0, 0, canvas.width, canvas.height)
+
+const gravity = 0.2
+
+class Sprite {
+    constructor({position, velocity}) {
+        this.position = position
+        this.velocity = velocity
+        this.height = 150
+    }
+
+    draw() {
+        c.fillStyle = 'red' 
+        c.fillRect(this.position.x, this.position.y, 50, this.height)
+    }
+
+    update() {
+        this.draw()
+
+
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+
+        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+          this.velocity.y = 0
+        } else this.velocity.y += gravity
+    } 
 }
-function handleKeyDown(event) {
-  switch (event.key) {
-    case "ArrowLeft":
-      square.style.left = (square.offsetLeft - 5) + "px";
-      break;
-    case "ArrowRight":
-      square.style.left = (square.offsetLeft + 5) + "px";
-      break;
-    case "ArrowUp":
-      square.style.top = (square.offsetTop - 5) + "px";
-      break;
-    case "ArrowDown":
-      square.style.top = (square.offsetTop + 5) + "px";
-      break;
-  }
+
+const player = new Sprite ({
+    position: {
+        x: 0,
+        y: 0
+    
+    },
+    velocity: {
+    x: 0,
+    y: 0
+    }
+})
+
+
+
+const enemy = new Sprite({
+    position: {
+        x: 400, 
+        y: 100
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
+})
+
+const keys = {
+    a: {
+        pressed: false
+    }
+    d: { 
+        pressed: false 
+    }
 }
+
+console.log(player)
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    c.fillStyle = 'black'
+    c.fillRect(0,0, canvas.width, canvas.height)
+    player.update()
+    enemy.update()
+
+    if (keys.a.pressed) {
+        player.velocity.x = -1
+    } else if (keys.d.pressed) {
+        player.velocity = 1 
+    }
+}
+
+animate()
+
+window.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case 'd':
+          keys.d.pressed = true
+          break
+        case 'a':
+          keys.a.pressed = true
+          break
+    }    
+    console.log(event.key);
+})
+
+window.addEventListener('keyup', (event) => {
+    switch (event.key) {
+        case 'd':
+          keys.d.pressed = false
+          break
+        case 'a':
+          keys.a.pressed = false
+          break
+    }    
+    console.log(event.key);
+
+
+})
+
+console.log("you are on index tutorial ver");
 
